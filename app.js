83d8475f6e78e12eff5a98d5f231c40f87129aed
asfandilyas <100092008+asfandilyas@users.noreply.github.com>
2022-05-15 14:04:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+//=== CHAP NO 26-30 ==//
+
+
+//==Ques no 1 ==//
+
+// var num = prompt("Enter Positive Integer Number");
+// var floor = Math.floor(num);
+// var round = Math.round(num);
+// var ceil = Math.ceil(num);
+// document.write(`Number: ${num} <br> 
+//                 Round off value: ${round} <br>
+//                 Floor value: ${floor} <br>
+//                 Ceil value: ${ceil}`)
+
+
+//==Ques no 2 ==//
+
+// var num = prompt("Enter Negative Integer Number");
+// var floor = Math.floor(num);
+// var round = Math.round(nu);
+// var ceil = Math.ceil(num);
+// document.write(`Number: ${num} <br> 
+//                 Round off value: ${round} <br>
+//                 Floor value: ${floor} <br>
+//                 Ceil value: ${ceil}`)
+
+
+//==Ques no 3 ==//
+
+// var num = -4;
+// var abs = Math.abs(num)
+// document.write(`The Absolute value of ${num} is ${abs}`)    
+
+
+///==Ques mo 04 ==///
+
+// var dice = Math.random() *6 +1
+// var dicefloor = Math.floor(dice)
+// // console.log(dicefloor)    
+// document.write(`Random dice is : ${dicefloor}`)
+
+
+///==Ques no 5 ==///
+
+// var coin = Math.random() * 2 +1 ;
+// coin = Math.floor(coin);
+// console.log(coin)
+// if(coin === 2){
+//     document.write(`${coin} <br> Random coin values : Heads`)
+// }else if(coin === 1){
+//     document.write(`${coin} <br> Random coin values : Tails`)
+// }    
+
+///===Ques no 6 ==///
+// var number = Math.random() * 100 + 1;
+// number = Math.floor(number);
+// document.write(`Random number b/w 1 to 100: ${number}`)    
+
+///== Ques no 7 ===//
+
+// var user = (prompt("Enter your weight in kilograms"));
+// document.write(`The weight of user is ${user} kilograms`)
+
+//== Ques no 8 ==//
+
+// var user = parseInt(prompt("Enter Secret Number:"));
+// var randomNum = Math.random() * 10 + 1;
+// randomNum = Math.floor(randomNum)
+// console.log(randomNum)
+// if(user === randomNum){
+//     alert("COngrats YOU WIN")}  
+// else{
+//     alert("Next time")}
+
+
+//== END ==//
+
